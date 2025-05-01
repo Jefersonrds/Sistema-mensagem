3f24b84e8bb40226ff485cb14f306ef92ddb6a7a
Jefersonrds <contatojefersonrds@gmail.com>
2025-05-01 13:49:28 -0300

    Abstrato: Definir que cada sistema de mensagem tem sua forma de enviar ou receber mensagem

--- src/ServicoMensagemInstantanea.java
+++ src/ServicoMensagemInstantanea.java
@@ -1,18 +1,8 @@
-public class ServicoMensagemInstantanea {
-    public void enviarMensagem(){
-        //verifica se esta conectada a internet
-        validarConectadoInternet();
-        System.out.println("Enviando mensagem.");
-        //depois de enviar mensagem, salva a mensagem no historico
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem(){
-        System.out.println("Reebendo mensagem.");
-    }
-    private void validarConectadoInternet(){
-        System.out.println("Validando se esta conectado a internet.");
-    }
-    private void salvarHistoricoMensagem(){
-        System.out.println("Salvando o historico da mensagem.");
-    }
+//Classe abstrata
+public abstract class ServicoMensagemInstantanea {
+    
+    //Metodos abstratos
+    //Metodos sem corpo, finalizados com ponto e virgula.
+    public abstract void enviarMensagem();
+    public abstract void receberMensagem();
 }
